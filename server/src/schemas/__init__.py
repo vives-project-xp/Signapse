c3a97c51e817ced6084d0af0a01e68e37c283534
SimonStnn <simon.stijnen.23@gmail.com>
2025-11-06 12:31:41 +0000

    Add keypoints schema

--- server/src/schemas/__init__.py
+++ server/src/schemas/__init__.py
@@ -1,10 +1,12 @@
 from .status import StatusResponse
 from .classes import ClassesResponse
 from .predict import PredictBody, PredictResponse
+from .keypoints import KeypointsBody
 
 __all__ = [
     "StatusResponse",
     "ClassesResponse",
     "PredictBody",
     "PredictResponse",
+    "KeypointsBody",
 ]
