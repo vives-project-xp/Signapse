ef340c04f7e76448ceac312b1d5acd8eac0a63db
SimonStnn <simon.stijnen.23@gmail.com>
2025-11-11 14:52:12 +0000

    Fix backend image handling and response format

--- server/src/schemas/__init__.py
+++ server/src/schemas/__init__.py
@@ -1,12 +1,12 @@
 from .status import StatusResponse
 from .classes import ClassesResponse
 from .predict import PredictBody, PredictResponse
-from .keypoints import KeypointsBody
+from .keypoints import KeypointsResponse
 
 __all__ = [
     "StatusResponse",
     "ClassesResponse",
     "PredictBody",
     "PredictResponse",
-    "KeypointsBody",
+    "KeypointsResponse",
 ]
