18b03ed9542803388e2c22b9c24b2747d35b9a52
SimonStnn <simon.stijnen.23@gmail.com>
2025-11-12 22:01:56 +0100

    Backend optimizations

--- server/src/main.py
+++ server/src/main.py
@@ -5,14 +5,24 @@ from websocket.connection_manager import ConnectionManager
 import routes
 import os
 import logging
+import warnings
 
 # Reduce TensorFlow/absl/glog verbosity to avoid noisy startup logs from
 # MediaPipe / TensorFlow. These environment variables MUST be set before
 # importing any module that may in turn import mediapipe or tensorflow.
-os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")
+os.environ.setdefault("TF_CPP_MIN_LOG_LEVEL", "3")  # 0=INFO, 1=WARNING, 2=ERROR, 3=FATAL
 os.environ.setdefault("GLOG_minloglevel", "3")
-# Optionally silence absl python logger too
+os.environ.setdefault("TF_ENABLE_ONEDNN_OPTS", "0")  # Suppress oneDNN custom operations
+os.environ.setdefault("MEDIAPIPE_DISABLE_GPU", "1")  # Disable GPU to reduce warnings
+# Suppress all TFLite delegate warnings
+os.environ.setdefault("TF_CPP_VMODULE", "inference_feedback_manager=0")
+
+# Silence Python warnings
+warnings.filterwarnings("ignore", category=Warning)
+# Silence absl python logger
 logging.getLogger("absl").setLevel(logging.ERROR)
+# Silence TensorFlow logger
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
 
 manager = ConnectionManager()
